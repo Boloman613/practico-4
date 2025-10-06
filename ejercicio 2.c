522274f0c6206423566c96e9c4029e8be53b95d9
Boloman613 <villegasaxel9412@gmail.com>
2025-10-06 18:37:06 -0400

    Add files via upload

---
+++ ejercicio 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int N, suma = 0;
+    printf("Ingrese un numero: ");
+    scanf("%d", &N);
+    while (N > 0) {
+        suma += N % 10;
+        N /= 10;
+    }
+    printf("La suma de los digitos es: %d\n", suma);
+    return 0;
+}
